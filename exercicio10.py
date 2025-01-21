132a6703359e58a3ceb36ac4c50d420323677f7f
Pedro <admpedro@hotmail.com>
2025-01-21 14:55:40 -0300

    exercicio 10

---
+++ exercicio10.py
@@ -0,0 +1,8 @@
+# 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada
+import math
+
+constante = math.pi
+raio = float(input("Digite o valor do raio: "))
+area = constante**2*raio
+
+print("A área do círculo é: ",area)
